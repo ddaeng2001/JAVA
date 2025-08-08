e3571e5483dfbb3084f1facb211ab5c06a224d88
ddaeng2001 <ddaeng2001@naver.com>
2025-08-08 15:47:52 +0900

    08/08

---
+++ JAVA01/src/Ch00/C01SystemOut.java
@@ -0,0 +1,41 @@
+package Ch00;
+
+public class C01SystemOut {
+
+	public static void main(String[] args) { //절차 지향 문법이기 떄문에 main안에 작성할 것!
+		
+		//System.out.print : 줄바꿈 없이 출력
+		//Escape 문자
+		// \n : 개행, 줄바꿈
+		// \b : 백스페이스
+		// \t : 탭길이(기본 8칸)만큼 커서 이동
+//		System.out.print("HELLO WORLD");
+//		System.out.print("HELLO WORLD");
+//		System.out.print("HELLO WORLD");
+//		System.out.print("HELLO WORLD");
+		//복사단축키 : ctrl + alt + ↓
+		
+		
+		
+		//System.out.printf : 형식을 지정하여 출력할 때 사용
+		//%d : 10진수 정수 서식문자
+		//%f : 10진수 실수 서식문자
+		//%c : 한문자('') 서식 문자
+		//%s : 문자열("") 서식 문자
+//		System.out.printf("%d %d %d \n", 10,20,30);
+//		System.out.printf("10,20,30 \n");
+//		System.out.printf("%f %f %f \n", 10.1,20.1,30.1);
+//		System.out.printf("%c %c %c \n", 'A','B','C');
+//		System.out.printf("%s %s %s \n", "This is", "String", "Test");
+//		System.out.printf("%d.%s : %d%s\n", 1, "국어", 100,"점");	//System.out.printf()
+		
+		
+		
+		//System.out.println
+		System.out.println("HELLOWORLD");
+		System.out.println("HELLOWORLD");
+		System.out.println("HELLOWORLD");
+		System.out.println("HELLOWORLD");
+	}
+
+}
