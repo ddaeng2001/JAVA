d797c17c81f1909570743aa8a7e73071f1db08fd
ddaeng2001 <ddaeng2001@naver.com>
2025-08-21 11:19:31 +0900

    08/21

--- JAVA02_MY/src/Ch06/C05InterfaceMain.java
+++ JAVA02_MY/src/Ch06/C05InterfaceMain.java
@@ -1,9 +1,17 @@
 package Ch06;
+//interface 다중상속 가능
+
+interface A{}
+interface B{}
+class C{}
+class D{}
+
+class E extends C implements A,B //순서는 지켜야함 extends -> implements 순
+{}
 
 public class C05InterfaceMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
